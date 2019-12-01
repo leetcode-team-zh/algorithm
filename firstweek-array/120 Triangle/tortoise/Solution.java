6b161be280c3d611c2ecdfc31c4754d3b3fc8810
周伟东 <1044755836@qq.com>
2019-12-01 18:20:24 +0800

    [120]三角形最小路径和

--- firstweek-array/120 Triangle/tortoise/Solution.java
+++ firstweek-array/120 Triangle/tortoise/Solution.java
@@ -24,12 +24,44 @@ public class Solution {
         return dp[0];
     }
 
+    public int minimumTotal2(List<List<Integer>> triangle) {
+        if(triangle == null || triangle.size() == 0){
+            return 0;
+        }
+
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+        dp[0][0] = triangle.get(0).get(0);
+        if(n == 1){
+            return dp[0][0];
+        }
+        int minValue = Integer.MAX_VALUE;
+
+        for(int i = 1 ; i < n; i++){
+            for(int j=0 ; j <= i; j++){
+                if(j == 0){
+                    dp[i][j] = triangle.get(i).get(j) + dp[i-1][j];
+                } else if(j == i){
+                    dp[i][j] = triangle.get(i).get(j) + dp[i-1][j -1];
+                }else{
+                    dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i-1][j],dp[i-1][j-1]);
+                }
+                if(i == n- 1){
+                    if(minValue > dp[i][j]){
+                        minValue = dp[i][j];
+                    }
+                }
+            }
+        }
+        return minValue;
+    }
+
     public static void main(String[] args) {
         List<List<Integer>> triangle = new ArrayList<>();
-        triangle.add(Arrays.asList(new Integer[]{2}));
-        triangle.add(Arrays.asList(new Integer[]{3,4}));
-        triangle.add(Arrays.asList(new Integer[]{6,5,7}));
-        triangle.add(Arrays.asList(new Integer[]{4,1,8,3}));
-        new Solution().minimumTotal(triangle);
+        triangle.add(Arrays.asList(new Integer[]{-10}));
+//        triangle.add(Arrays.asList(new Integer[]{3,4}));
+//        triangle.add(Arrays.asList(new Integer[]{6,5,7}));
+//        triangle.add(Arrays.asList(new Integer[]{4,1,8,3}));
+        new Solution().minimumTotal2(triangle);
     }
 }
