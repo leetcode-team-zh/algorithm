cf97fa8256474accfbd7c856588fb09d67cb3174
周伟东 <1044755836@qq.com>
2020-02-09 17:26:48 +0800

    201. 数字范围按位与

---
+++ ninth-week/201 bitwise and of numbers range/tortoise/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.d201;
+
+public class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        while (n > m) {
+            n = n & (n - 1);
+        }
+        return n;
+    }
+}
