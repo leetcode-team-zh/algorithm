b71dcd9312678b3eeb18dda812986054fc52f410
周伟东 <1044755836@qq.com>
2020-05-29 07:38:29 +0800

    House Robber

---
+++ One question per day/day67/House Robber/tortoise/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.d198;
+
+public class Solution {
+    public int rob(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+
+        int length = nums.length;
+        if(length == 1){
+            return nums[0];
+        }
+
+        int first = nums[0];
+        int second = Math.max(nums[0],nums[1]);
+        for(int  i = 2 ; i < length; i++){
+            int temp = second;
+            second = Math.max(first + nums[i],second);
+            first = temp;
+        }
+        return second;
+    }
+}
