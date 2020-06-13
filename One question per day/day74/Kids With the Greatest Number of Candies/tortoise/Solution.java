e3c2c1d8e8d21382cb29ff62faa96c4fa5e59d20
周伟东 <1044755836@qq.com>
2020-06-13 19:45:08 +0800

    Kids With the Greatest Number of Candies

---
+++ One question per day/day74/Kids With the Greatest Number of Candies/tortoise/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.d1431;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int n = candies.length;
+
+        //找出最大值
+        int maxCandies = 0;
+        for (int i = 0; i < n; ++i) {
+            maxCandies = Math.max(maxCandies, candies[i]);
+        }
+        //判断是否拥有水果最多
+        List<Boolean> ret = new ArrayList<Boolean>();
+        for (int i = 0; i < n; ++i) {
+            ret.add(candies[i] + extraCandies >= maxCandies);
+        }
+        return ret;
+    }
+}
